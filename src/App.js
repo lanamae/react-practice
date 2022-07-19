739e2d83d0cde830128142dc9d4d1026ae17b509
Lana Mae Galvez <lanamaegalvez15@gmail.com>
2022-07-19 13:40:23 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = ()  =>{
-    const count = 0
+    const [count, setCount] = useState(0)
+    const [user, setUser] = useState('')
+
+    const increment = () =>{
+        setCount(count +1 )
+    }
+
+    const handleOnChANGE = (e) =>{
+        setUser(e.target.value)
+    }
+
+    console.log("user:", user)
 
     return(
-        <div>
-            <div>Count: {}</div>
-            <button>Increment</button>
-        </div>
+        < >
+            <div>Count: {count}</div>
+            <button id="" onClick={increment}>Increment</button>
+            <br/>
+            <input name="user" onChange={handleOnChANGE} value={user}/>
+        </>
         
 
     )
